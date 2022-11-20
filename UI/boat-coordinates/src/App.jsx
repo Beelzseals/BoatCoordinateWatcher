746898d61bd2c52a65c89d634912b5a8b482c01e
Beelzseals <88729970+Beelzseals@users.noreply.github.com>
2022-11-20 19:08:19 +0100

    add random coordinates remove OldApp.jsx

--- UI/boat-coordinates/src/App.jsx
+++ UI/boat-coordinates/src/App.jsx
@@ -1,34 +1,82 @@
-import React, { useState, useEffect, useRef } from 'react';
-import { Map, View } from 'ol';
-import TileLayer from 'ol/layer/Tile';
-import OSM from 'ol/source/OSM';
-import 'ol/ol.css';
+import React, { useState, useEffect, useRef } from "react";
+import { Map, View } from "ol";
+import TileLayer from "ol/layer/Tile";
+import OSM from "ol/source/OSM";
+import "ol/ol.css";
+import { Fill, RegularShape, Stroke, Style } from "ol/style";
 
 function App() {
-    const [map, setMap] = useState();
-    const mapElement = useRef();
-    const mapRef = useRef();
-    mapRef.current = map;
 
-    useEffect(() => {
-        const initialMap = new Map({
-          target: mapElement.current,
-            layers: [
-                new TileLayer({
-                    source: new OSM(),
-                }),
-            ],
-            view: new View({
-                center: [0, 0],
-                zoom: 0,
-            }),
-        });
-        setMap(initialMap);
-    }, []);
+  const [currentMap, setCurrentMap] = useState(new Map());
+  const [coords, setCoords] = useState([])
+  const mapElement = useRef();
+  const mapRef = useRef();
+  mapRef.current = currentMap;
 
-    return (
-      <div style={{height:'100vh',width:'100%'}} ref={mapElement} className="map-container" />
-    );
+  const stroke = new Stroke({ color: "black", width: 2 });
+  const fillRed = new Fill({ color: "red" });
+  const fillBlack = new Fill({color: "black"})
+
+  const styles = {
+    'square': new Style({
+        image: new RegularShape({
+            fill: fillBlack,
+            stroke: stroke,
+            points: 4,
+            radius: 10,
+            angle: Math.PI / 4
+        }),
+    }),
+    'square2': new Style({
+        image: new RegularShape({
+            fill: fillRed,
+            stroke: stroke,
+            points: 4,
+            radius: 10,
+            angle: Math.PI/4
+        })
+    })
+  }
+
+  const styleKey = "square"
+
+  const count = 250;
+const features = new Array(count);
+const e = 4500000;
+for (let i = 0; i < count; ++i) {
+  const coordinates = [2 * e * Math.random() - e, 2 * e * Math.random() - e];
+  features[i] = new Feature(new Point(coordinates));
+  features[i].setStyle(
+    styles[styleKeys[Math.floor(Math.random() * styleKeys.length)]]
+  );
+}
+
+const source = new VectorSource({
+  features: features,
+});
+
+const vectorLayer = new VectorLayer({
+  source: source,
+});
+
+  useEffect(() => {
+    const initialMap = new Map({
+      target: mapElement.current,
+      layers: [
+        new TileLayer({
+          source: new OSM(),
+        }),
+      ],
+      view: new View({
+        center: [0, 0],
+        zoom: 2,
+      }),
+    });
+    setCurrentMap(initialMap);
+  }, []);
+
+  
+  return <div id="map" ref={mapElement} className="map-container"></div>;
 }
 
 export default App;
